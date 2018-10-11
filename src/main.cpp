9d9327178f07b558eb7eaa0f7f316e4da0b34c1f
Joshua Sutton <jjs910@hotmail.com>
2018-10-11 15:26:24 +1100

    Add command line argument checking

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,37 @@
 // 11/10/2018
 //
 
+#include <cstdlib>
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Check if the right amount of arguments have been entered
+    if(argc != 4)
+    {
+        cerr << "Not enough arguments given." << endl;
+        cerr << "./Genetics [n] [codon file] [data file]" << endl << endl;
+        cerr << "[n] - A number, 3 or 4, to distinguish whether we are dealing with an AlphabetThree or AlphabetFour case" << endl;
+        cerr << "[codon file] - Text file describing the mapping of codons to amino acids." << endl;
+        cerr << "[data file] - Text file containing the data to operate on." << endl;
+        return 1;
+    }
+
+    // Get the alphabet number
+    int alphabetNum = atoi(argv[1]);
+    // Check if the alphabet number is either 3 or 4
+    if(alphabetNum != 3 && alphabetNum != 4)
+    {
+        cerr << "Incorrect Alphabet number given." << endl;
+        cerr << alphabetNum << " was entered, should be 3 or 4." << endl;
+        return 2;
+    }
+
+    // Grab the file names for the 2 required text files
+    string codonFileName = string(argv[2]);
+    string dataFileName = string(argv[3]);
+
     return 0;
 }
