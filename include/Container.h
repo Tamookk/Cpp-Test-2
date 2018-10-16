41fa9a7588d632cd7bd20f66c689bd120795d6ff
Josh <jjs910@hotmail.com>
2018-10-16 22:35:57 +1100

    Report when end of data reached without encountering a stop instruction

--- include/Container.h
+++ include/Container.h
@@ -11,26 +11,12 @@
 #include <fstream>
 #include <iostream>
 #include <map>
-// Class needs to:
-// > Report on and validate the length of the content
-//   - Must be a multiple of 3
-// > Report on the symbol distribution for the container
-//   - Whatever that means
-// > List the complete content of the container.
-//   - 60 elements per line, like the sample files
-// > Display all the encoded active amino acid chains
-//   - Read and process codons from the data sequence (3 symbols at a time)
-//   - When we reach a start instruction:
-//     1. Output the amino acids using their their names separated by dashes
-//     2. Keep going until a stop instruction is reached
-//     3. The number of amino acids in each chain should be reported
-//   - If the end of the data is reached without a stop instruction, this should be reported
-//   - Report the number of codons NOT displayed between start and stop instructions, and at the beginning + end as appropriate
+
+// TODO: If the end of the data is reached without a stop instruction, this should be reported
 
 using namespace std;
 
-template <typename T>
-
+template <class T>
 class Container
 {
 private:
@@ -211,6 +197,11 @@ void Container<T>::processCodons(multimap<string, string> mappedCodons)
         // Add the char to the codon string
         codon += c;
         charNum++;
+    }
+
+    if(hasStarted)
+    {
+        cout << "End of data reached without encountering a \"Stop\" instruction" << endl;
     }
 
     if(skippedCodons > 0)
